4fb3fb379d0e7fe24de85d53c1c5550939977fbe
Kyle McKell <me@kylemckell.com>
2022-12-28 16:47:31 -0700

    putting more code away from index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import type { StringObject } from "./sharedTypes.ts"
 
-import { hexArrToHSL, hexArrToRGB } from "./formatColors.ts"
-import { writeCSSFile, writeJSONFile } from "./writeToFile.ts"
+import { writeToFiles } from "./writeToFile.ts"
 
 const baseDir = "./palettes"
 const distDir = "./dist"
@@ -19,16 +18,7 @@ for await (const dirEntry of dir) {
   const json: StringObject = JSON.parse(fileNoComments)
   const hexArray = Object.entries(json)
 
-  const hslArray = hexArrToHSL(hexArray)
-  await writeJSONFile(hslArray, fileName, `${distDir}/json-hsl`)
-  await writeCSSFile(hslArray, fileName, `${distDir}/css-hsl`)
-
-  // we have rgb too ayo
-  const rgbArray = hexArrToRGB(hexArray)
-  await writeJSONFile(rgbArray, fileName, `${distDir}/json-rgb`)
-  await writeCSSFile(rgbArray, fileName, `${distDir}/css-rgb`)
-
-  // may as well write the hex variants since we have it
-  await writeCSSFile(hexArray, fileName, `${distDir}/css-hex`)
-  await writeJSONFile(hexArray, fileName, `${distDir}/json-hex`)
+  await writeToFiles({ colorType: "rgb", hexArray, fileName, dirName: distDir })
+  await writeToFiles({ colorType: "hex", hexArray, fileName, dirName: distDir })
+  await writeToFiles({ colorType: "hsl", hexArray, fileName, dirName: distDir })
 }
--- src/writeToFile.ts
+++ src/writeToFile.ts
@@ -1,4 +1,39 @@
+import { hexArrToHSL, hexArrToRGB } from "./formatColors.ts"
 import type { StringObject } from "./sharedTypes.ts"
+
+type WriteToFilesProps = {
+  colorType: "rgb" | "hex" | "hsl"
+  hexArray: [string, string][]
+  fileName: string
+  dirName: string
+}
+
+export const writeToFiles = async ({
+  colorType,
+  hexArray,
+  fileName,
+  dirName,
+}: WriteToFilesProps) => {
+  switch (colorType) {
+    case "rgb": {
+      const rgbArray = hexArrToRGB(hexArray)
+      await writeJSONFile(rgbArray, fileName, `${dirName}/json-rgb`)
+      await writeCSSFile(rgbArray, fileName, `${dirName}/css-rgb`)
+      break
+    }
+    case "hsl": {
+      const hslArray = hexArrToHSL(hexArray)
+      await writeJSONFile(hslArray, fileName, `${dirName}/json-hsl`)
+      await writeCSSFile(hslArray, fileName, `${dirName}/css-hsl`)
+      break
+    }
+    case "hex": {
+      await writeCSSFile(hexArray, fileName, `${dirName}/css-hex`)
+      await writeJSONFile(hexArray, fileName, `${dirName}/json-hex`)
+      break
+    }
+  }
+}
 
 export const writeJSONFile = async (
   colorArray: string[][],
