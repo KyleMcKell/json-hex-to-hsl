46e837361c5fd705927d85208c5e4693c3fd345c
Kyle McKell <me@kylemckell.com>
2023-01-01 13:36:22 -0700

    added some more sample palettes, and also made it so if a non hex color value is passed in, it just returns the color

--- src/formatColors.ts
+++ src/formatColors.ts
@@ -2,6 +2,7 @@ import { hexToHSL, hexToRGB } from "./colorChange.ts"
 
 export const hexArrToHSL = (fileArr: [string, string][]) => {
   const newColors = fileArr.map((color) => {
+    if (!color[1].startsWith("#")) return color
     const [name, hex] = color
     const { hsla } = hexToHSL(hex)
     return [name, hsla]
@@ -11,6 +12,7 @@ export const hexArrToHSL = (fileArr: [string, string][]) => {
 
 export const hexArrToRGB = (fileArr: [string, string][]) => {
   const newColors = fileArr.map((color) => {
+    if (!color[1].startsWith("#")) return color
     const [name, hex] = color
     const { rgb } = hexToRGB(hex)
     return [name, rgb]
